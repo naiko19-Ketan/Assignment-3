b4803c8f87198cc2f427b6a61f40a3706e2987f6
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-06-07 13:46:31 +0530

    Update Q1.py

--- Input/Q1.py
+++ Input/Q1.py
@@ -1,6 +1,9 @@
-print("The program exits when an incorrect expression is entered or user enters quit")
-while True :
-    i=input("Enter the expression :")
-    if i.lower() == 'quit' or i=='':
-        break
-    print("Output is:", eval(i))
+number=int(input())
+temp=number
+place_value=0
+binary=0
+while(temp > 0):
+    binary = ((temp%2)*(10**place_value)) + binary
+    temp = int(temp/2)
+    place_value += 1
+print("Binary of {x} is: {y}".format(x=number,y=binary))
